6b5ce4cc5e3b8a7b599a3b651276b3cea2fdaaa0
Rojhat Men <rojhatmen@hotmail.com>
2024-04-23 13:01:22 +0300

    "Add Review"

--- src/main/java/com/rojo/libraryapp/config/MyDataRestConfig.java
+++ src/main/java/com/rojo/libraryapp/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.rojo.libraryapp.config;
 
 import com.rojo.libraryapp.entity.Book;
+import com.rojo.libraryapp.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -18,8 +19,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.POST,HttpMethod.PATCH,HttpMethod.DELETE,HttpMethod.PUT};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
---
+++ src/main/java/com/rojo/libraryapp/dao/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.rojo.libraryapp.dao;
+
+import com.rojo.libraryapp.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewRepository extends JpaRepository<Review,Long> {
+
+    Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
+
+}
