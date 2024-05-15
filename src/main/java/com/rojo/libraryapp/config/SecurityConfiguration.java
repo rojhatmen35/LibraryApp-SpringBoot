8a1578f48b4dc7be492c5d8962d936f77c18ce3f
Rojhat Men <rojhatmen@hotmail.com>
2024-05-15 16:26:23 +0300

    "Review operations added"

--- src/main/java/com/rojo/libraryapp/config/SecurityConfiguration.java
+++ src/main/java/com/rojo/libraryapp/config/SecurityConfiguration.java
@@ -20,7 +20,7 @@ public class SecurityConfiguration {
 
         http.authorizeRequests(configurer ->
                 configurer
-                        .antMatchers("/api/books/secure/**")
+                        .antMatchers("/api/books/secure/**","/api/reviews/secure/**")
                         .authenticated())
                 .oauth2ResourceServer()
                 .jwt();
---
+++ src/main/java/com/rojo/libraryapp/service/ReviewService.java
@@ -0,0 +1,54 @@
+package com.rojo.libraryapp.service;
+
+import com.rojo.libraryapp.dao.BookRepository;
+import com.rojo.libraryapp.dao.ReviewRepository;
+import com.rojo.libraryapp.entity.Review;
+import com.rojo.libraryapp.requestmodels.ReviewRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Service
+@Transactional
+public class ReviewService {
+
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(ReviewRepository reviewRepository){
+        this.reviewRepository=reviewRepository;
+    }
+
+    public void postReview(String userEmail, ReviewRequest reviewRequest)throws Exception{
+        Review validateReview=reviewRepository.findByUserEmailAndBookId(userEmail,reviewRequest.getBookId());
+        if (validateReview !=null){
+            throw new Exception("Review already created");
+        }
+
+        Review review=new Review();
+        review.setBookId(reviewRequest.getBookId());
+        review.setRating(reviewRequest.getRating());
+        review.setUserEmail(userEmail);
+
+        if (reviewRequest.getReviewDescription().isPresent()) {
+            review.setReviewDescription(reviewRequest.getReviewDescription().map(Object::toString).orElse(null));
+        }
+
+        review.setDate(Date.valueOf(LocalDate.now()));
+        reviewRepository.save(review);
+
+    }
+
+    public Boolean userReviewListed(String userEmail, Long bookId) {
+        Review validateReview= reviewRepository.findByUserEmailAndBookId(userEmail,bookId);
+        if (validateReview != null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
